6da92b534bdab07014af74e9a02bd8a32e5e5c1a
W4ngatang <thewansta@gmail.com>
2013-12-22 10:35:49 -0800

    finished problem 1

--- exercises-more/exercises.py
+++ exercises-more/exercises.py
@@ -2,11 +2,7 @@
 # Return the number of words in the string s. Words are separated by spaces.
 # e.g. num_words("abc def") == 2
 def num_words(s):
-    words = s.split()
-    if not words:
-	return 0
-    else:
-	return len(words)
+    return len(s.split())
 
 # PROB 2
 # Return the sum of all the numbers in lst. If lst is empty, return 0.
